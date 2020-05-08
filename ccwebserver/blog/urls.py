649e69316f340547758aa1a9b2fb93148797358a
zill4 <justcrisp@outlook.com>
2020-05-07 19:50:02 -0700

    wiping the slate clean

--- ccwebserver/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
-from blog import views
- 
-urlpatterns = [
-    url(r'^blog/$', views.BlogList.as_view(), name='blog-list'),
-    url(r'^blog/(?P<pk>[0-9]+)/$', views.BlogDetail.as_view(), name='blog-detail'),
-]
