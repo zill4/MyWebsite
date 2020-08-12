7c7c8a14eef90b6f9140b7fbf8f2cb58fabc3605
zill4 <justcrisp@outlook.com>
2020-08-11 21:41:53 -0700

    Fixed home page sort of

--- crisp-code-site/src/pages/Home.svelte
+++ crisp-code-site/src/pages/Home.svelte
@@ -47,9 +47,13 @@
 	</button> -->
   </div>
 
- <div class="bio">
-		<img src="./images/headshot.jpg" class="img-rounded" alt="Justin Crisp 2020" width="25%" height="25%"/> 
-			<h1>Hi my name is Justin Crisp</h1>
+ <div class="row">
+
+	<div class="col-3 col-s-3 menu">
+			<img src="./images/headshot.jpg" class="img-rounded" alt="Justin Crisp 2020" width="25%" height="25%"/> 
+	</div>
+	<div class="col-6 col-s-9">
+			<h1 class="intro">Hi my name is Justin Crisp</h1>
 			<p class="par">
 			Thanks for visiting my website! I'm 23, love building cool projects, working with passionate people, and challenging the status quo.
 			After moving to The Bay Area in 2015, I subsequently enrolled in De Anza College studying Computer Engineering for about 3 years.
@@ -58,6 +62,13 @@
 			Around May 2019, I competed in, and won a hackathon sponsored by Samsung Bixby team. Since then I've been working in Developer Relations on the Bixby team.
 			It's now 2020, almost 5 years since I arrived in the Bay Area. This year I look to challenge myself with new technologies, and drive to build great things, with amazing people, through coding.
 			</p>
+	</div>
+	<div class="col-3 col-s-12">
+		<div class="aside">
+			<Poke/>
+
+		</div>
+	</div>
   </div>
 	<!-- <p>{count} doubled is {doubled}</p>
 	<p>{numbers.join(' + ')} = {sum}</p>
@@ -65,4 +76,105 @@
 		Add a number
 	</button> -->
 <style>
+.intro {
+	padding: 10px;
+}
+.par {
+	letter-spacing: 0.1em;
+	line-height: 2.7em;
+}
+* {
+  box-sizing: border-box;
+}
+
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+[class*="col-"] {
+  float: left;
+  padding: 15px;
+}
+
+
+.menu ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu img {
+  padding: 8px;
+  margin-bottom: 7px;
+  background-color: #33b5e5;
+  color: #ffffff;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+	padding-top: 20px;
+	padding-right: 20px;
+	padding-bottom: 20px;
+	padding-left: 20px;
+	border-radius: 50%;
+	height: 75%;
+	width: 75%;
+}
+
+.menu li:hover {
+  background-color: #0099cc;
+}
+
+.aside {
+
+  padding: 15px;
+  color: black;
+  text-align: center;
+  font-size: 14px;
+
+}
+
+
+
+/* For mobile phones: */
+[class*="col-"] {
+  width: 100%;
+}
+
+@media only screen and (min-width: 600px) {
+  /* For tablets: */
+  .col-s-1 {width: 8.33%;}
+  .col-s-2 {width: 16.66%;}
+  .col-s-3 {width: 25%;}
+  .col-s-4 {width: 33.33%;}
+  .col-s-5 {width: 41.66%;}
+  .col-s-6 {width: 50%;}
+  .col-s-7 {width: 58.33%;}
+  .col-s-8 {width: 66.66%;}
+  .col-s-9 {width: 75%;}
+  .col-s-10 {width: 83.33%;}
+  .col-s-11 {width: 91.66%;}
+  .col-s-12 {width: 100%;}
+}
+@media only screen and (min-width: 768px) {
+  /* For desktop: */
+  .col-1 {width: 8.33%;}
+  .col-2 {width: 16.66%;}
+  .col-3 {width: 25%;}
+  .col-4 {width: 33.33%;}
+  .col-5 {width: 41.66%;}
+  .col-6 {width: 50%;}
+  .col-7 {width: 58.33%;}
+  .col-8 {width: 66.66%;}
+  .col-9 {width: 75%;}
+  .col-10 {width: 83.33%;}
+  .col-11 {width: 91.66%;}
+  .col-12 {width: 100%;}
+}
+
+.jumbotron {
+		display: flex;
+		justify-content: center;
+		padding: 125px;
+		background: #0099cc;
+}
 </style>
--- crisp-code-site/src/pages/Resume.svelte
+++ crisp-code-site/src/pages/Resume.svelte
@@ -28,27 +28,12 @@ function handleTabClick (item){
     <div class="profile">
         <img src="./images/headshot.jpg" class="img-rounded" alt="Justin Crisp 2020" width="75%" height="75%"> 
         <h1>{data.firstName} {data.middleName} {data.lastName}</h1>
-
     </div>
             <h2>{data.header}</h2>
             <h3>{data.location}</h3>
         <br>
             <h4>{data.objective}</h4>
-      <!-- <ul class="nav nav-pills nav-stacked">
-        <li class="active"><a href="#section1">Bio</a></li>
-        <li><a href="#section2">Friends</a></li>
-        <li><a href="#section3">Family</a></li>
-        <li><a href="#section3">Photos</a></li>
-      </ul> -->
       <br>
-      <!-- <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search Blog..">
-        <span class="input-group-btn">
-          <button class="btn btn-default" type="button">
-            <span class="glyphicon glyphicon-search"></span>
-          </button>
-        </span>
-      </div> -->
     </div>
 
     <div class="col-sm-9">
@@ -71,54 +56,7 @@ function handleTabClick (item){
     {:else if tab[4].status == 'active'}
     <Volunteer />
     {/if}
-<!-- 
-      <h4><small>RECENT POSTS</small></h4>
-      <hr>
-      <h2>Officially Blogging</h2>
-      <h5><span class="glyphicon glyphicon-time"></span> Post by John Doe, Sep 24, 2015.</h5>
-      <h5><span class="label label-success">Lorem</span></h5><br>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <hr>
-
-      <h4>Leave a Comment:</h4>
-      <form role="form">
-        <div class="form-group">
-          <textarea class="form-control" rows="3" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-success">Submit</button>
-      </form>
-      <br><br>
-      
-      <p><span class="badge">2</span> Comments:</p><br> -->
-      
       <div class="row">
-        <!-- <div class="col-sm-2 text-center">
-          <img src="bandmember.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-        </div>
-        <div class="col-sm-10">
-          <h4>Anja <small>Sep 29, 2015, 9:12 PM</small></h4>
-          <p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <br>
-        </div>
-        <div class="col-sm-2 text-center">
-          <img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-        </div> -->
-        <!-- <div class="col-sm-10">
-          <h4>John Row <small>Sep 25, 2015, 8:25 PM</small></h4>
-          <p>I am so happy for you man! Finally. I am looking forward to read about your trendy life. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <br>
-          <p><span class="badge">1</span> Comment:</p><br>
-          <div class="row">
-            <div class="col-sm-2 text-center">
-              <img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar">
-            </div>
-            <div class="col-xs-10">
-              <h4>Nested Bro <small>Sep 25, 2015, 8:28 PM</small></h4>
-              <p>Me too! WOW!</p>
-              <br>
-            </div>
-          </div>
-        </div> -->
       </div>
     </div>
   </div>
@@ -136,6 +74,8 @@ function handleTabClick (item){
 		padding-bottom: 50px;
 		padding-left: 50px;
 		border-radius: 50%;
+    height: 25%;
+    width: 25%;
 }
 .experience {
     text-align: left;
