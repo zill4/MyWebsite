ce8ae6ccc98da0aaeb8c4e3ad8ed5335e750fc19
zill4 <justcrisp@outlook.com>
2020-08-26 09:28:35 -0700

    typo correction

--- crisp-code-site/src/pages/Home.svelte
+++ crisp-code-site/src/pages/Home.svelte
@@ -54,7 +54,7 @@
 	<div class="col-6 col-s-9">
 			<h1 class="intro">Hi my name is Justin Crisp</h1>
 			<p class="par">
-			Thanks for visiting my website! I'm love building cool projects, working with passionate people, and challenging the status quo.
+			Thanks for visiting my website! I love building cool projects, working with passionate people, and challenging the status quo.
 			After moving to The Bay Area in 2015, I subsequently enrolled in De Anza College studying Computer Engineering for about 3 years.
 			Once I completed the majority of my Computer Science classes, and started working part-time at RingCentral (A UCAAS company competing with Slack & Zoom), I decided to "drop out".
 			I imediately enrolled at 42 Silicon Valley, a Computer Science school for those looking to focus on coding. The curicullum is in C, there are no teachers, and no classes.
