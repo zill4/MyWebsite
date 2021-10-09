37d059bbc1e3da6fb0bdb4170e0ee2895d3f6dcd
zill4 <justcrisp@outlook.com>
2021-10-09 13:06:09 -0700

    mockup

---
+++ crispcode/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+          <h1>CrispCode.io</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+  <v-card>
+    <v-tabs
+      background-color="deep-purple accent-4"
+      center-active
+      dark
+    >
+      <v-tab>One</v-tab>
+      <v-tab>Two</v-tab>
+      <v-tab>Three</v-tab>
+
+    </v-tabs>
+  </v-card>
+    </v-app-bar>
+
+    <v-main>
+      <Home/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Home from './components/Home';
+
+export default {
+  name: 'App',
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ crispcode/src/components/Home.vue
@@ -0,0 +1,208 @@
+<template>
+  <v-container>
+      <v-carousel
+    cycle
+    height="400"
+    hide-delimiter-background
+    show-arrows-on-hover
+      >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        :color="colors[i]"
+        height="100%"
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="text-h2">
+            {{ slide }} Slide
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+    <v-row>
+      <v-col>
+        <v-card class="d-inline-flex pa-2" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Warlok</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Share </v-btn>
+
+            <v-btn color="orange" text> Explore </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="d-flex flex-row-reverse">
+        <v-card class="d-inline-flex flex-row-reverse pa-2" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>ZillA</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Share </v-btn>
+
+            <v-btn color="orange" text> Explore </v-btn>
+          </v-card-actions>
+        </v-card>      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card class="d-inline-flex pa-2" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Cy8er</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Share </v-btn>
+
+            <v-btn color="orange" text> Explore </v-btn>
+          </v-card-actions>
+        </v-card>      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="d-flex flex-row-reverse">
+        <v-card class="d-inline-flex flex-row-reverse pa-2" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>SocialEye</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Share </v-btn>
+
+            <v-btn color="orange" text> Explore </v-btn>
+          </v-card-actions>
+        </v-card>      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  data: () => ({
+              colors: [
+          'indigo',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+          'deep-purple accent-4',
+        ],
+        slides: [
+          'Warlok',
+          'ZillA',
+          'SocialEye',
+          'Cy8er',
+          'Fifth',
+        ],
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader",
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify",
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
+      },
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com",
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com",
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify",
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs",
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify",
+      },
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer",
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+      },
+    ],
+  }),
+};
+</script>
