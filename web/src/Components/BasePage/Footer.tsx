616e7096989dc3cde40b37b1d36c4313aafb4237
zill4 <justcrisp@outlook.com>
2022-03-13 21:29:22 -0700

    need to add mobile view

--- web/src/Components/BasePage/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { PageProps } from '../../Models/Util'
-
-export const Footer: React.FC<PageProps> = ({ title, children }) => {
-  return (
-    <footer className="h-20 rounded-md bg-gray-500">
-      <div className="flex text-white place-content-center">{children}</div>
-    </footer>
-  )
-}
--- web/src/Components/BasePage/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { PageProps } from '../../Models/Util'
-
-export const Header: React.FC<PageProps> = ({ title, children }) => {
-  return (
-    <header className="flex align-middle justify-center rounded-md bg-gray-400 w-full">
-      {children}
-    </header>
-  )
-}
